d6490678789d4bd1a26e4372ebca86980f0816a8
babasahi <babesalehmahfoud@gmail.com>
2026-01-30 13:03:45 +0100

    feat: Implement a redesigned transfer screen UI including send/request money options, quick transfer sections, a QR code feature, and a new `TransferOption` widget.

--- lib/views/screens/transfer_screen.dart
+++ lib/views/screens/transfer_screen.dart
@@ -6,42 +6,156 @@ class TransferScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Transfer'),
-        centerTitle: true,
-        backgroundColor: Colors.blueAccent,
-        leading: IconButton(
-          icon: const Icon(Icons.arrow_back),
-          onPressed: () {
-            Navigator.pop(context);
-          },
-        ),
-      ),
+      backgroundColor: Colors.white,
       body: SafeArea(
         child: Column(
           children: [
-            Row(
-              children: [
-                Container(
-                  height: MediaQuery.of(context).size.height * 0.35,
-                  width: MediaQuery.of(context).size.width * 0.5,
-                  decoration: BoxDecoration(
-                    color: Colors.blueAccent,
-                    borderRadius: BorderRadius.circular(10),
+            Container(
+              width: MediaQuery.of(context).size.width,
+              height: MediaQuery.of(context).size.height * 0.28,
+              padding: EdgeInsets.all(8),
+              decoration: BoxDecoration(
+                color: Color.fromRGBO(0, 188, 213, 1),
+              ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Icon(
+                        Icons.arrow_forward_ios,
+                        color: Colors.transparent,
+                      ),
+
+                      Text(
+                        'تحويل الأموال',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      Icon(Icons.arrow_forward, color: Colors.white),
+                    ],
+                  ),
+                  SizedBox(height: MediaQuery.of(context).size.height * 0.07),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+
+                    children: [
+                      TransferOption(
+                        title: 'طلب المال',
+                        imageAsset:
+                            'assets/icons/fundtransfer_requestmoney.png',
+                        color: Color.fromRGBO(254, 193, 11, 1),
+                      ),
+                      TransferOption(
+                        title: 'ارسل المال',
+                        imageAsset: 'assets/icons/fundtransfer_sendmoney.png',
+                        color: Color.fromRGBO(3, 133, 150, 1),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              height: MediaQuery.of(context).size.height * 0.1,
+              width: MediaQuery.of(context).size.width,
+              color: Colors.grey.shade200,
+              child: Center(
+                child: Text(
+                  'التحويل السريع',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 16,
                   ),
                 ),
-                Container(
-                  height: MediaQuery.of(context).size.height * 0.35,
-                  width: MediaQuery.of(context).size.width * 0.5,
-                  decoration: BoxDecoration(
-                    color: Colors.blueAccent,
-                    borderRadius: BorderRadius.circular(10),
+              ),
+            ),
+            Container(
+              padding: EdgeInsets.symmetric(
+                vertical: 22,
+              ),
+              child: Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Image.asset(
+                      'assets/icons/ic_qr.png',
+                      height: MediaQuery.of(context).size.height * 0.1,
+                      width: MediaQuery.of(context).size.width * 0.1,
+                      color: Color.fromRGBO(0, 188, 213, 1),
+                    ),
+                    Text('رمز QR'),
+                  ],
+                ),
+              ),
+            ),
+            Container(
+              height: MediaQuery.of(context).size.height * 0.1,
+              width: MediaQuery.of(context).size.width,
+              color: Colors.grey.shade200,
+              child: Center(
+                child: Text(
+                  'التحويل السريع',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 16,
                   ),
                 ),
-              ],
+              ),
             ),
           ],
         ),
+      ),
+    );
+  }
+}
+
+class TransferOption extends StatelessWidget {
+  const TransferOption({
+    super.key,
+    required this.title,
+    required this.imageAsset,
+    required this.color,
+  });
+
+  final String title;
+  final String imageAsset;
+  final Color color;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: MediaQuery.of(context).size.height * 0.12,
+      width: MediaQuery.of(context).size.width * 0.45,
+      decoration: BoxDecoration(
+        color: color,
+      ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Center(
+            child: RotatedBox(
+              quarterTurns: 2,
+              child: Image.asset(
+                imageAsset,
+                height: MediaQuery.of(context).size.height * 0.05,
+                width: MediaQuery.of(context).size.width * 0.25,
+              ),
+            ),
+          ),
+          Text(
+            title,
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 16,
+            ),
+          ),
+        ],
       ),
     );
   }
