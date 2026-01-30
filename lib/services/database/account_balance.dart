5cc1f020985007e1daa0f877ee5a2c852fd2de80
babasahi <babesalehmahfoud@gmail.com>
2026-01-30 20:57:36 +0100

    changes

--- lib/services/database/account_balance.dart
+++ lib/services/database/account_balance.dart
@@ -1,27 +1,6 @@
-import 'dart:convert';
-import 'package:demo_project/services/caching.dart';
-import 'package:demo_project/services/database/register.dart';
-import 'package:http/http.dart' as http;
-
 Future<double?> getAccountBalance() async {
   try {
-    String token = await getToken() ?? '';
-    Map<String, String> headers = {
-      'Content-Type': 'application/json',
-      'Authorization': 'Bearer $token',
-    };
-    final response = await http.get(
-      Uri.parse(backendUrl + '/account/balance'),
-      headers: headers,
-    );
-    if (response.statusCode == 200) {
-      print(response.body);
-      var data = jsonDecode(response.body);
-      return data['balance'];
-    } else {
-      print(response.body);
-      return null;
-    }
+    return null;
   } catch (e) {
     print(e.toString());
     return null;
--- lib/services/database/login.dart
+++ lib/services/database/login.dart
@@ -1,38 +1,7 @@
-import 'dart:convert';
-
 import 'package:demo_project/models/bankily_user.dart';
-import 'package:demo_project/services/database/register.dart';
-import 'package:http/http.dart' as http;
 
 Future<BankilyUser?> loginUser(BankilyUser user) async {
-  try {
-    Map<String, String> headers = {
-      'Content-Type': 'application/json',
-    };
-    Map<String, String> body = {
-      "phone": user.phoneNumber,
-      "password": user.password,
-    };
-    print(body);
-    print(headers);
-    final response = await http.post(
-      Uri.parse(backendUrl + '/auth/login'),
-      headers: headers,
-      body: jsonEncode(body),
-    );
-    if (response.statusCode == 200) {
-      print(response.body);
-      return BankilyUser(
-        phoneNumber: jsonDecode(response.body)['user']['phone'],
-        password: user.password,
-        name: jsonDecode(response.body)['user']['full_name'],
-        token: jsonDecode(response.body)['token'],
-      );
-    } else {
-      print(response.body);
-      return null;
-    }
-  } catch (e) {
+  try {} catch (e) {
     print(e.toString());
     return null;
   }
